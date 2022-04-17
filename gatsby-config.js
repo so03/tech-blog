cd2fa479edc583021c25df11397408c963d3b288
sou <sasaki.sou@samurai-adways.net>
2022-04-17 10:25:04 +0900

    modify branch name of deploying to pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,16 @@ module.exports = {
     title: `tech blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+  ],
   pathPrefix: `/tech-blog`
 };
