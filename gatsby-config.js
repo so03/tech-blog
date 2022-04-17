dc6d491efa2ce511f592d5ad6cfbf58a1001e20b
sou <sasaki.sou@samurai-adways.net>
2022-04-17 09:58:45 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `tech blog`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet"]
+};
