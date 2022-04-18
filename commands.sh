4b013feb766d85435e2d5cba6e2ef41940891c2d
sou <sasaki.sou@samurai-adways.net>
2022-04-18 11:46:29 +0900

    public to docs

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-mkdir public/hello-github-pages
-pandoc src/articles/2022-04-18.md -o public/hello-github-pages/index.html
+mkdir docs/hello-github-pages
+pandoc src/articles/2022-04-18.md -o docs/hello-github-pages/index.html
